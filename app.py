84fd919b95243f604075b7f3ad8d5718006e82e8
Arseny <20096ninja@gmail.com>
2023-08-30 21:44:39 +0300

    flask API updated

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
 
 from utils import parsedData, rawData, imgFromBytes
+from scraper import scrapping
 
 app = Flask(__name__)
 
 
 @app.route('/api/test')
 def index():
-    return parsedData(rawData(imgFromBytes(request.data)))
+    return scrapping(parsedData(rawData(imgFromBytes(request.data))))
 
 
 if __name__ == '__main__':
